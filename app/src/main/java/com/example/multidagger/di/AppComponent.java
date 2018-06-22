01533d65755002d06d97422ee427510dbdca6fd9
Young Ik Cho <youngik.cho@samsung.com>
2018-06-22 11:10:52 +0900

    removed unnecessary component
    Signed-off-by: Young Ik Cho <youngik.cho@samsung.com>

--- app/src/main/java/com/example/multidagger/di/AppComponent.java
+++ app/src/main/java/com/example/multidagger/di/AppComponent.java
@@ -10,7 +10,7 @@ import javax.inject.Singleton;
 
 @Singleton
 @Component(modules = {
-    AndroidInjectionModule.class, AppModule.class, ServiceBuildersModule.class, MainActivityModule.class,
+    AndroidInjectionModule.class, AppModule.class, ServiceBuildersModule.class,
 })
 public interface AppComponent {
 
